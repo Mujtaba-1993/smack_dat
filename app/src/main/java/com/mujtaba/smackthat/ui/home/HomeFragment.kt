c3cca5845cefec53cdc9da8a7136820ba13b044f
Mujtaba-1993 <64317539+Mujtaba-1993@users.noreply.github.com>
2020-05-30 14:21:51 +0300

    master

---
+++ app/src/main/java/com/mujtaba/smackthat/ui/home/HomeFragment.kt
@@ -0,0 +1,18 @@
+package com.mujtaba.smackthat.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.mujtaba.smackthat.R
+
+class HomeFragment : Fragment() {
+
+    private lateinit var homeViewModel: HomeViewModel
+
+
+}
